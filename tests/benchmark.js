2fa06f585c75c31138ad566ecedfe334697c7137
Andre Staltz <andre@staltz.com>
2023-06-13 13:52:42 +0300

    improve benchmark to be a bit more fair

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -6,12 +6,12 @@ run(async () => {
   utils.logMem();
   let x = 'deadbeef';
   const B = '12345678';
+  const Bn = BigInt('0x'+B);
 
   await compare('poseidon', 5000, {
     hsg88_circomlib() {
       const an = BigInt('0x' + x);
-      const bn = BigInt('0x' + B);
-      const xn = circomlibjs.poseidon([an, bn]);
+      const xn = circomlibjs.poseidon([an, Bn]);
       x = xn.toString(16);
     },
     rs_poseidon_wasm() {
