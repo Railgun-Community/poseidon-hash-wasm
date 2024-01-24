79e2a89e8d20ad5011bb174143df5473466b8a07
Andre Staltz <andre@staltz.com>
2024-01-24 12:04:47 +0200

    add hand-written .d.ts

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+* @param {Array<BigInt>} args
+* @returns {BigInt}
+*/
+export function poseidon(args: Array<BigInt>): BigInt;
+
+/**
+* @param {Array<string>} args
+* @returns {string}
+*/
+export function poseidonHex(args: Array<BigInt>): BigInt;
+
+export default () => {};
+export const initSync: () => void;
