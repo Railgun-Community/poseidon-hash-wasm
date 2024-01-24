fa4e1f3bef22af23797e5aba608b810a4e3b72bd
Andre Staltz <andre@staltz.com>
2024-01-24 14:38:59 +0200

    refactor commonjs index.js

--- index.js
+++ index.js
@@ -3,28 +3,22 @@ const wasm = require('./pkg-cjs/poseidon_hash_wasm');
 const SCALAR_FIELD =
   21888242871839275222246405745257275088548364400416034343698204186575808495617n;
 
-/**
- * @param {Array<bigint>} inputs
- * @returns {bigint}
- */
-function poseidon(inputs) {
-  const hexInputs = inputs.map((input) => {
-    if (input > SCALAR_FIELD) {
-      return (input % SCALAR_FIELD).toString(16);
-    } else {
-      return input.toString(16);
-    }
-  });
-  const hexOutput = wasm.poseidon(hexInputs);
-  return BigInt('0x' + hexOutput);
-}
-
 module.exports = {
-  poseidon,
+  poseidon(inputs) {
+    const hexInputs = inputs.map((input) => {
+      if (input > SCALAR_FIELD) {
+        return (input % SCALAR_FIELD).toString(16);
+      } else {
+        return input.toString(16);
+      }
+    });
+    const hexOutput = wasm.poseidon(hexInputs);
+    return BigInt('0x' + hexOutput);
+  },
   poseidonHex: wasm.poseidon,
   default() {
     return Promise.resolve();
   },
   initSync: () => {},
   __esModule: true,
-}
+};
